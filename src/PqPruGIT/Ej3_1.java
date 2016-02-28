a1dc7faef39edfbe12dedc0fb14de8a90e3c1523
pablo <pablo@192.168.1.36>
2016-02-29 00:07:12 +0100

    Nuevo commit a dia 29 de febrero

---
+++ src/PqPruGIT/Ej3_1.java
@@ -0,0 +1,21 @@
+package PqPruGIT;
+
+
+public class Ej3_1 {
+
+
+    public static void main(String[] args) 
+    {
+        char borracho;
+        
+        borracho = 'S';
+        
+        if(borracho == 'S')
+        {
+            System.out.println("No conduzcas.");
+            System.out.println("Estás borracho.");
+        }
+        System.out.println("FIN");
+    }
+
+}
---
+++ src/PqPruGIT/Ej3_2.java
@@ -0,0 +1,20 @@
+package PqPruGIT;
+
+public class Ej3_2 
+{
+    
+    public static void main(String[] args) 
+    {
+        char borracho;
+        
+        borracho = 'S';
+        
+        if ((borracho == 'S') || (borracho == 's'))
+        {
+            System.out.println("No conduzcas.");
+            System.out.println("Estás borracho.");
+        }
+        
+        System.out.println("FIN");
+    }
+}
